7e340da67ccf0727929bc26e45a81865b8a9896a
raiod12 <victorb.correa1@gmail.com>
2021-01-15 18:06:21 -0300

    Adicionando View index, controler e model mocado

--- Blog/Blog/Controllers/HomeController.cs
+++ Blog/Blog/Controllers/HomeController.cs
@@ -1,37 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Blog.Models;
 
 namespace Blog.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
-            return View();
-        }
+            var listaDePosts = new List<Post>()
+            {
+                    new Post() { Titulo = "Harry Potter 1", Resumo = "Pedra Filosofal", Categoria = "Filme, Livro" },
+                    new Post() { Titulo = "Cassino	Royale", Resumo = "007", Categoria = "Filme" },
+                    new Post() { Titulo = "Monge e o Executivo", Resumo = "Romance sobre Liderança", Categoria  = "Livro" },
+                    new Post() { Titulo = "New York, New York", Resumo = "Sucesso de Frank Sinatra", Categoria = "Música" }
+        };
 
-        public IActionResult Privacy()
-        {
+            ViewBag.Posts = listaDePosts;
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
