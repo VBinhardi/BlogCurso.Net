e2c85d26cfb7f1b77788b50fcb0e9b043f5e685e
raiod12 <victorb.correa1@gmail.com>
2021-01-20 12:29:31 -0300

    Adicionando funcionalidade de adicionar post com lista mockada

--- Blog/Blog/Controllers/PostController.cs
+++ Blog/Blog/Controllers/PostController.cs
@@ -9,6 +9,18 @@ namespace Blog.Controllers
 {
     public class PostController : Controller
     {
+        private IList<Post> lista; 
+
+        public PostController()
+        {
+            this.lista = new List<Post>
+            {
+                new Post() { Titulo = "Harry Potter 1", Resumo = "Pedra Filosofal", Categoria = "Filme, Livro" },
+                    new Post() { Titulo = "Cassino	Royale", Resumo = "007", Categoria = "Filme" },
+                    new Post() { Titulo = "Monge e o Executivo", Resumo = "Romance sobre Liderança", Categoria  = "Livro" },
+                    new Post() { Titulo = "New York, New York", Resumo = "Sucesso de Frank Sinatra", Categoria = "Música" }
+            };
+        }
         public IActionResult Index()
         {
             var listaDePosts = new List<Post>()
@@ -20,6 +32,17 @@ namespace Blog.Controllers
         };
 
             return View(listaDePosts);
+        }
+
+        public IActionResult Novo()
+        {
+            return View();
+        }
+
+        public IActionResult Adiciona(Post post)
+        {
+            lista.Add(post);
+            return View("Index", lista);
         }
     }
 }
